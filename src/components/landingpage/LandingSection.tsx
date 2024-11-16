8129026417dabd20ea904047a26422a7164596b2
diasraphael <diasraphael88@gmail.com>
2024-11-16 20:45:47 +0100

    updated audio player

--- src/components/landingpage/LandingSection.tsx
+++ src/components/landingpage/LandingSection.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import Header from "../header/Header";
-import Slider from "./Slider";
-import SocialIcons from "./SocialIcons";
+import React from 'react';
+import Header from '../header/Header';
+import Slider from './Slider';
+import SocialIcons from './SocialIcons';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
+const Player = () => (
+  <div className="audio-player">
+    <AudioPlayer
+      autoPlay
+      src="https://radio.chandrabose.no/listen/viduthalaifm/live"
+      onPlay={(e) => console.log('onPlay')}
+    />
+  </div>
+);
 const LandingSection = () => {
   return (
     <div className="min-h-screen">
       <Header></Header>
       <SocialIcons></SocialIcons>
+      <Player></Player>
       <Slider></Slider>
     </div>
   );
