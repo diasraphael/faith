0e3aee72467f757288f16fbc88041b0ab31e22b6
diasraphael <diasraphael88@gmail.com>
2025-04-13 14:53:06 +0200

    updated the new video

--- src/components/landingpage/VideoSection.tsx
+++ src/components/landingpage/VideoSection.tsx
@@ -9,22 +9,25 @@ const VideoSection = () => {
       <div className="video-container-sermon">
         <div className="content-box">
           <h2>Welcome to Faith Baptist Church</h2>
-          <p>Our Latest Sermon: Self-Sympathy - A Spiritual Challenge</p>
-          <p>Scripture References: 1 Kings 19:1-18, Proverb 28:13 </p>
+          <p>Our Latest Sermon: Spiritual Sickness: Despising God’s Call</p>
+          <p>Scripture References: Genesis 25:24-34 Matthew 22:1-14 </p>
           <p>
-            Self-sympathy is a spiritual ailment that can divert us from God's
-            blessings and His plans for our lives. It can mislead us from the
-            path to achieving our goals. The story of Elijah in 1 Kings 19:1-18
-            serves as a powerful example. Despite being a mighty servant of God,
-            Elijah succumbed to self-pity. Yet, in his moments of weakness and
-            despair, God provided him with strength, enabling him to journey for
-            40 days. This demonstrates that when God sustains us, we receive
-            supernatural strength. This message encourages believers to rely on
-            God's grace to overcome self-pity and to live a life that honors
-            Him, regardless of the struggles, battles, or difficult days we may
-            face.
+            Despising God's Call: Despising means considering something as
+            negligible or worthless. If we view our salvation and God’s call as
+            unimportant, the consequences can be regrettable. Biblical Examples:
+            Esau's Birthright: When Esau was hungry, he sold his blessed
+            birthright for food. As a consequence, he is mentioned as a
+            fornicator (a negative example of faith) and could not receive the
+            blessing he despised (Hebrews 12:16-17). Parable of the Wedding
+            Feast: Jesus told a parable about a king inviting people to his
+            son’s wedding feast. All the invited guests despised the call,
+            leading the king to become furious and destroy them. Jesus
+            concluded, "Many are called, but few are chosen." Accepting God's
+            Call: People who accept His call to become His children (John 1:12)
+            and prioritize God in everything are chosen to inherit God’s
+            kingdom. God keeps His covenant and mercy with them (Nehemiah 1:5).
           </p>
-          <p>To learn more about Self-sympathy check out the video below</p>
+          <p>To learn more about, check out the video below</p>
           <p>Visit us - Faith Baptist Church, Sunday 11AM</p>
           <p>Our Location - Stålfjæra 20, 0975 Oslo, Norway</p>
           <button
@@ -45,7 +48,7 @@ const VideoSection = () => {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/UK1uOufmomg?si=QWlxQDayop5COdlE"
+              src="https://www.youtube.com/embed/1z4nLTRDhmQ?si=pA5tjhvXn1xsauej"
               title="Faith Baptist Church Service"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
